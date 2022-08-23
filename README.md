47ff321145431ef4e32e4c83cc287e4a729da2d1
harin <harin0802@naver.com>
2022-08-23 16:52:27 +0900

    final_setting

---
+++ README.md
@@ -0,0 +1,10 @@
+1.가상환경 생성 python -m venv myvenv
+2.가상환경 구동 source myvenv/Scripts/activate
+3.가상환경 위에 장고 설치하기 pip install django
+4.장고 설치 확인하기 pip list -> 확인
+5.실행하기 python manage.py runserver
+*Pillow 설치하라고 할 경우 안내문에 따라 설치하기
+
+배포 링크 : 'harin.pythonanywhere.com'
+settings.py 설정
+ALLOWED_HOSTS = ['harin.pythonanywhere.com']
--- shoppingmallProject/settings.py
+++ shoppingmallProject/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-import os
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,11 +9,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-om^n#dq4gfe_*%2x1%+ps85)^3y(dpom_5w+uf%32d$wm1z4w^'
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ['harin.pythonanywhere.com'] #배포 링크
 ALLOWED_HOSTS = []
